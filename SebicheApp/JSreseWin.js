07d38176601e866b91ba22ededc6d3ff02ac6319
Jerson Vargas <112907333+jersvarg@users.noreply.github.com>
2022-12-14 18:17:09 -0500

    page load

---
+++ SebicheApp/JSreseWin.js
@@ -0,0 +1,8 @@
+function f01() {
+	  let nombreRsv = document.getElementById("Name").value;
+	  let telefonoRsv = document.getElementById("phone").value;
+	  let personasRsv = document.getElementById("person").value;
+	  let fechasRsv = document.getElementById("fecha").value;
+	  let horaRsv = document.getElementById("time").value;
+	  alert(horaRsv);
+}
